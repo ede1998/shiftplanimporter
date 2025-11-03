8ab8190135307ff75fd2c53d973c04ca4b119cb0
ede1998 <online@erik-hennig.me>
2025-11-03 17:21:18 +0100

    feat: TimeFrameView

---
+++ app/src/main/java/me/erik_hennig/shiftplanimporter/ui/TimeFrameView.kt
@@ -0,0 +1,73 @@
+package me.erik_hennig.shiftplanimporter.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.erik_hennig.shiftplanimporter.ui.theme.ShiftPlanImporterTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+@Composable
+fun TimeFrameView(
+    modifier: Modifier = Modifier,
+    upcomingMonths: List<Date>
+) {
+    val monthFormat = SimpleDateFormat("MMMM yyyy", Locale.getDefault())
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(text = "Select Time Frame", style = MaterialTheme.typography.headlineMedium)
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = 2.dp,
+            color = Color.Gray
+        )
+
+        upcomingMonths.forEach { date ->
+            Button(
+                onClick = { /* TODO: Handle month selection */ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = monthFormat.format(date))
+            }
+        }
+
+        Button(
+            onClick = { /* TODO: Handle custom time frame selection */ },
+            enabled = false,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Select Custom Range (Not implemented yet)")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TimeFrameViewPreview() {
+    val upcomingMonths = mutableListOf<Date>()
+    val calendar = Calendar.getInstance()
+
+    for (i in 0..3) {
+        upcomingMonths.add(calendar.time)
+        calendar.add(Calendar.MONTH, 1)
+    }
+    ShiftPlanImporterTheme {
+        TimeFrameView(upcomingMonths = upcomingMonths)
+    }
+}
